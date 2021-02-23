8984cccff0b960ac7806e5cd44962d413d736412
maxmclarentaylor <max.mclaren.taylor@outlook.com>
2021-02-23 10:53:52 +0000

    added in coding results

--- src/search.js
+++ src/search.js
@@ -1,3 +1,18 @@
 module.exports = (data, query) => {
-  // write your code here
+    var value = data
+    if(query["settings.live"]){
+      let array = []
+      data.forEach(((dataValue, index) => {
+        if(dataValue["settings"]["live"]){
+        if(query["name"] === dataValue["name"]){
+            array.splice(0,0,dataValue)
+          }
+          else{
+            array.push(dataValue)
+          }
+        }
+      }))
+      value = array
+    }
+    return value
 }
