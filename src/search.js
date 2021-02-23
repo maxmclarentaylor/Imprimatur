56d0f3a9230988cab87e96ca3e374f1e9a16fc82
maxmclarentaylor <max.mclaren.taylor@outlook.com>
2021-02-23 15:50:50 +0000

    added in later changes

--- src/search.js
+++ src/search.js
@@ -1,18 +1,30 @@
 module.exports = (data, query) => {
     var value = data
-    if(query["settings.live"]){
-      let array = []
-      data.forEach(((dataValue, index) => {
-        if(dataValue["settings"]["live"]){
-        if(query["name"] === dataValue["name"]){
-            array.splice(0,0,dataValue)
-          }
-          else{
-            array.push(dataValue)
-          }
+    var keys = Object.keys(query)
+    if(keys.length > 0){
+    keys.map((keyValue,index) => {
+        if(keyValue.indexOf(".") !== -1){
+            var array = []
+            let arrayOfKeys = keyValue.split(".")
+              data.forEach((object,index2) => {
+                if(object[arrayOfKeys[0]][arrayOfKeys[1]] === query[keyValue]){
+                      if(query[keys[index + 1]] === object[keys[index + 1]]){
+                        array.splice(0,0,object)
+                      }
+                      else{
+                        array.push(object)
+                      }
+                }
+              })
+              value = array
         }
-      }))
-      value = array
-    }
+        else if(keyValue === "id"){
+          var index = value.findIndex(object => object.id === query[keyValue])
+          var valueToAddBackIn = value.slice(index,index + 1)
+          value.splice(index,1)
+          value.splice(0,0,valueToAddBackIn[0])
+        }
+      }) 
+    }   
     return value
 }
--- tests/search.test.js
+++ tests/search.test.js
@@ -9,12 +9,24 @@ describe('Search Test', () => {
     })
     expect(results[0].id).toEqual(1)
   })
+  test(`Can find users by id 2`, () => {
+    const results = search(data, {
+      id: 4
+    })
+    expect(results[0].id).toEqual(4)
+  })
 
   test(`Will handle dot notation in paths correctly`, () => {
     const results = search(data, {
       'settings.live': true
     })
     expect(results.length).toBe(3)
+  })
+  test(`Will handle dot notation in paths correctly round 2`, () => {
+    const results = search(data, {
+      'settings.lastActive': '2021-02-16'
+    })
+    expect(results.length).toBe(2)
   })
 
   test(`When a blank query object is specified it will return all data`, () => {
@@ -29,5 +41,14 @@ describe('Search Test', () => {
     })
     expect(results[0]).toHaveProperty('name', 'Bill')
     expect(results[0]).toHaveProperty('settings.live', true)
+  })
+
+  test(`Handle multiple search paths as a logical AND 2`, () => {
+    const results = search(data, {
+      'settings.lastActive': '2021-02-15',
+      id: 4
+    })
+    expect(results[0]).toHaveProperty('name', 'Joana')
+    expect(results[0]).toHaveProperty('settings.live', false)
   })
 })
